0e593207085ed3e272d4b135adf929d544f55687
Broderick Higby <brody@datahinge.org>
2020-09-14 16:50:04 -0700

    separated the video functions

--- CameraView/RecordSwingView.swift
+++ CameraView/RecordSwingView.swift
@@ -15,24 +15,31 @@ enum RecordingEventState {
     case stopRecording
     case cancel
 }
+
+
 enum RecordingTextState: String {
     case bodyInScreenText = "Stand so that you can see your whole body in screen"
     case timerInstructions = "Get ready to swing after the timer"
-    case timer = "2"
+    case timer = "5"
     case swing = "Swing!"
 }
-
 
 class RecordingState: ObservableObject {
     @Published var countdownFinished = false
     @Published var state = RecordingEventState.findingPerson
     @Published var cancelRecording = false
+    @Published var personInView: Bool = false
+    @Published var captureSessionType: CaptureType = .videoStreamForProcessing
+    @Published var recording: Bool = false
+    
+    enum CaptureType {
+        case recordMovie, videoStreamForProcessing
+    }
 }
-
 
 struct RecordSwingView: View {
     @ObservedObject var recordingState = RecordingState()
-    @State private var countdownTimer = 5
+    
     @State private var golferInView = false
     @State private var recording = false
     @State private var showTimerText = false
@@ -40,27 +47,37 @@ struct RecordSwingView: View {
     @State private var recordingText = RecordingTextState.bodyInScreenText
     
     var body: some View {
-        ZStack {
-            CameraView(recordingState: recordingState)
-                .padding()
-        }.overlay(
-            VStack {
-                overlayEvents()
+        VStack {
+            Group {
+                if self.recordingState.state == .findingPerson {
+                    CameraStreamView(model: recordingState)
+                } else {
+                    VideoRecordingView(model: recordingState)
+                }
             }
-            
-        )
-            .onReceive(recordingState.$state, perform: { state in
-                if state == .countdownTimer {
-                    withAnimation(.easeInOut(duration: 1.0)) {
-                        self.eventView = .countdownTimer
-                    }
-                }
-                else if state == .recording {
-                    withAnimation(.easeInOut(duration: 1.0)) {
-                        self.eventView = .recording
-                    }
-                }
+            Button(action: {
+                self.recordingState.recording.toggle()
+                print("recording state: \(self.recordingState.recording)")
+            }, label:
+                {
+                    Text("Record")
             })
+
+        }
+        .edgesIgnoringSafeArea(.all)
+//        .overlay(overlayEvents())
+        .onReceive(recordingState.$state, perform: { state in
+            if state == .countdownTimer {
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    self.eventView = .countdownTimer
+                }
+            }
+            else if state == .recording {
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    self.eventView = .recording
+                }
+            }
+        })
     }
     
     func overlayEvents() -> AnyView {
@@ -74,6 +91,142 @@ struct RecordSwingView: View {
         default:
             return AnyView(EmptyView())
         }
+    }
+}
+
+
+struct EventOne: View {
+    @ObservedObject var recordingState: RecordingState
+    
+    var body: some View {
+        ZStack {
+            GradientOverlay()
+            TextDisplayOverCameraView(text: RecordingTextState.bodyInScreenText.rawValue)
+        }
+        .onAppear(perform: { // Temporary
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.recordingState.state = .countdownTimer
+            }
+        })
+        .onReceive(recordingState.$personInView, perform: { detected in
+            if detected {
+                self.recordingState.personInView = true
+            }
+        })
+    }
+}
+
+
+struct EventTwo: View {
+    @ObservedObject var recordingState: RecordingState
+    @State private var showTimerText = false
+    
+    var body: some View {
+        ZStack {
+            GradientOverlay()
+            if showTimerText {
+                CountdownTimerView(currentState: self.recordingState)
+            } else {
+                TextDisplayOverCameraView(text: RecordingTextState.timerInstructions.rawValue)
+            }
+        }.onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.showTimerText = true
+            }
+        })
+    }
+}
+
+
+struct EventThree: View {
+    var body: some View {
+        ZStack {
+            GradientOverlay()
+            TextDisplayOverCameraView(text: RecordingTextState.swing.rawValue)
+        }
+    }
+}
+
+struct CountdownTimerView: View {
+    @ObservedObject var currentState: RecordingState
+    @State var timeRemaining = 5
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+
+            TextDisplayOverCameraView(text: "\(timeRemaining)")
+            //            .foregroundColor(.white)
+            //            .font(.largeTitle)
+            //            .multilineTextAlignment(.center)
+        }
+            .onReceive(timer) { _ in
+                if self.timeRemaining > 0 {
+                    self.timeRemaining -= 1
+                } else {
+                    self.currentState.countdownFinished = true
+                    print(self.currentState.countdownFinished)
+                    self.currentState.state = .recording // Transition states
+                }
+            }
+    }
+}
+
+
+struct TextDisplayOverCameraView: View {
+    var text: String?
+    
+    var body: some View {
+        Text(text ?? "")
+            .foregroundColor(.white)
+            .font(.system(size: 46.0))
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+}
+
+struct HalfSquareOverlayView: View {
+    @State private var animationAmount: CGFloat = 1
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State private var angle: Double = 0
+    
+    var body: some View {
+        VStack {
+            Rectangle()
+                .trim(from: 0, to: 0.5)
+                .stroke(Color.red, lineWidth: 20)
+                .frame(width: 200, height: 200)
+                //.transition(AnyTransition.move(edge: .leading))
+                .animation(
+                    
+                    Animation.linear(duration: 1)
+                        .repeatForever(autoreverses: true)
+            )
+            //.rotationEffect(.degrees(angle))
+        }
+    }
+}
+
+struct GradientOverlay: View {
+    var gradient: LinearGradient {
+        LinearGradient(
+            gradient: Gradient(
+                colors: [Color.gray.opacity(0.7), Color.gray.opacity(0.7)]),
+            startPoint: .bottom,
+            endPoint: .top)
+    }
+
+    var body: some View {
+        ZStack {
+            Rectangle().fill(gradient)
+//            VStack {
+//                Spacer()
+//                Rectangle()
+//                    .fill(Color.white)
+//                    .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+//            }
+        }
+        .allowsHitTesting(false)
     }
 }
 
--- CameraView/VideoRecordingView.swift
+++ CameraView/VideoRecordingView.swift
@@ -9,29 +9,15 @@
 import SwiftUI
 import AVKit
 
-
+typealias ImageBufferHandler = ((_ imageBuffer: CMSampleBuffer) -> ())
 
 
 struct VideoRecordingView: UIViewRepresentable {
-    let model: CameraViewModel
+    let model: RecordingState
     
     func makeUIView(context: UIViewRepresentableContext<VideoRecordingView>) -> VideoPreviewView {
-        let recordingView = VideoPreviewView()
+        let recordingView = VideoPreviewView(frame: .zero, bridgeModel: model)
         recordingView.bridgeModel = self.model
-        
-        recordingView.onComplete = {
-            self.model.onComplete = true
-        }
-        
-        recordingView.onRecord = { timeLeft, currentRecordingTime in
-            self.model.timeLeft = timeLeft
-            self.model.recording = true
-        }
-        
-        recordingView.onReset = {
-            self.model.recording = false
-            self.model.timeLeft = 30
-        }
         return recordingView
     }
     
@@ -41,23 +27,187 @@ struct VideoRecordingView: UIViewRepresentable {
 }
 
 
-
+/// For Recording Videos
 class VideoPreviewView: UIView {
-    var bridgeModel: CameraViewModel?
+    var bridgeModel: RecordingState?
     private var captureSession: AVCaptureSession?
-    private var countdownTimer: Timer?
+    /// For Recording
     let videoFileOutput = AVCaptureMovieFileOutput()
     var recordingDelegate:AVCaptureFileOutputRecordingDelegate!
+
+    init(frame: CGRect, bridgeModel: RecordingState) {
+        super.init(frame: frame)
+        self.bridgeModel = bridgeModel
+        requestCameraPermissions()
+        setupLiveCamera()
+        
+        if bridgeModel.captureSessionType == .videoStreamForProcessing {
+            setupVideoDataOutput()
+        }
+    }
     
-    var recorded = 0
-    var recordingTimeLimit = 30
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
     
-    var onRecord: ((Int, Int)->())? // countdown & duration
-    var onReset: (() -> ())?
-    var onComplete: (() -> ())?
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
-    init() {
-        super.init(frame: .zero)
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    /// Requests camera access if not granted already
+    func requestCameraPermissions() {
+        var allowedAccess = false
+        let blocker = DispatchGroup()
+        blocker.enter()
+        AVCaptureDevice.requestAccess(for: .video) { flag in
+            allowedAccess = flag
+            blocker.leave()
+        }
+        blocker.wait()
+        
+        if allowedAccess == false {
+            print("No camera Access") // TODO: Create User notification
+        }
+    }
+    
+    func setupLiveCamera() {
+         // setup session
+         let session = AVCaptureSession()
+         session.beginConfiguration()
+         guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                         for: .video, position: .front) else {
+                                                             print("Built in Camera is not active") // TODO: Add Notification to user
+                                                             return
+         }
+         guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice), session.canAddInput(videoDeviceInput) else {
+             print("No Camera Detected")
+             return
+         }
+         
+         session.addInput(videoDeviceInput)
+         session.commitConfiguration()
+         self.captureSession = session
+     }
+    
+    //NOTE: The only difference between this and LiveCameraView are startTimers() & startRecording() are called here
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        recordingDelegate = self
+                
+        if let _ = self.superview {
+            self.videoPreviewLayer.session = self.captureSession
+            self.videoPreviewLayer.videoGravity = .resizeAspectFill
+            self.captureSession?.startRunning()
+            // Record if set
+            if bridgeModel!.captureSessionType == .recordMovie {
+                self.startRecording()
+            }
+            
+        } else {
+            self.captureSession?.stopRunning()
+        }
+    }
+}
+/// This delegate is for recording videos
+extension VideoPreviewView: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        print(outputFileURL.absoluteString)
+        
+        if bridgeModel!.recording {
+            self.startRecording()
+        } else {
+            stopRecording()
+            // TODO: Check video saving completion, make sure resources are dumped
+        }
+        
+    }
+    
+    func startRecording(){
+        print("Recording: \(videoFileOutput.isRecording)")
+        captureSession?.addOutput(videoFileOutput)
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let filePath = documentsURL.appendingPathComponent("temporaryVideoFile")
+        videoFileOutput.startRecording(to: filePath,
+                                       recordingDelegate: recordingDelegate)
+    }
+    
+    func stopRecording(){
+        videoFileOutput.stopRecording()
+        print("Recording: \(videoFileOutput.isRecording)")
+    }
+}
+
+
+/// This extension is used for streaming frames for machine learning
+extension VideoPreviewView: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func setupVideoDataOutput() {
+        // setup video output
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        // If running slowly, set the dispatch to run on the main UI thread
+        let queue = DispatchQueue(label: "com.queue.videosamplequeue")
+        videoDataOutput.setSampleBufferDelegate(self, queue: queue)
+        guard captureSession!.canAddOutput(videoDataOutput) else {
+            fatalError()
+        }
+        captureSession!.addOutput(videoDataOutput)
+        videoConnection = videoDataOutput.connection(with: .video)
+    }
+    
+    // TODO: Stop Buffer once person is found
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if connection.videoOrientation != .portrait {
+            connection.videoOrientation = .portrait
+            return
+        }
+    
+        if let imageBufferHandler = imageBufferHandler {
+            imageBufferHandler(sampleBuffer)
+            
+//            DispatchQueue.main.async {
+//                self.bridgeModel?.personInView = true // << assign here as needed
+            // TODO: When personIsInView, Kill resources and open VideoRecorder
+//            }
+        }
+    }
+}
+
+
+struct CameraStreamView: UIViewRepresentable {
+    let model: RecordingState
+    
+    func makeUIView(context: UIViewRepresentableContext<CameraStreamView>) -> CameraPreviewView {
+        let recordingView = CameraPreviewView(frame: .zero, bridgeModel: model)
+        recordingView.bridgeModel = self.model
+        return recordingView
+    }
+    
+    func updateUIView(_ uiViewController: CameraPreviewView, context: UIViewRepresentableContext<CameraStreamView>) {
+        
+    }
+}
+
+
+/// For Streaming the camera to process the frames (i.e. with ML)
+class CameraPreviewView: UIView {
+    var bridgeModel: RecordingState?
+    private var captureSession: AVCaptureSession?
+
+    
+    /// For Streaming
+    private var videoConnection: AVCaptureConnection!
+    var videoDataDelegate: AVCaptureVideoDataOutputSampleBufferDelegate!
+    var imageBufferHandler: ImageBufferHandler?
+    
+
+    init(frame: CGRect, bridgeModel: RecordingState) {
+        super.init(frame: frame)
+        self.bridgeModel = bridgeModel
         requestCameraPermissions()
         setupLiveCamera()
     }
@@ -112,74 +262,67 @@ class VideoPreviewView: UIView {
     //NOTE: The only difference between this and LiveCameraView are startTimers() & startRecording() are called here
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
-        recordingDelegate = self
-        
-        startTimers()
-        
+        videoDataDelegate = self
+                
         if let _ = self.superview {
             self.videoPreviewLayer.session = self.captureSession
-            self.videoPreviewLayer.videoGravity = .resizeAspect
+            self.videoPreviewLayer.videoGravity = .resizeAspectFill
             self.captureSession?.startRunning()
-            self.startRecording()
+            
         } else {
             self.captureSession?.stopRunning()
         }
     }
 }
-extension VideoPreviewView: AVCaptureFileOutputRecordingDelegate {
-    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
-        print(outputFileURL.absoluteString)
-    }
-    
-    private func onTimerFires() {
-        print("Start Recording: \(videoFileOutput.isRecording)")
-        recordingTimeLimit -= 1
-        recorded += 1
-        onRecord?(recordingTimeLimit, recorded)
-        
-        if(recordingTimeLimit == 0){
-            stopRecording()
-            countdownTimer?.invalidate()
-            countdownTimer = nil
-            onComplete?()
-            videoFileOutput.stopRecording()
-        }
-    }
-    
-    func startTimers(){
-        if countdownTimer == nil {
-            countdownTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] (timer) in
-                self?.onTimerFires()
-            }
-        }
-    }
-    
-    func startRecording(){
-        captureSession?.addOutput(videoFileOutput)
-        
-        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let filePath = documentsURL.appendingPathComponent("temporaryVideoFile")
-        
-        videoFileOutput.startRecording(to: filePath,
-                                       recordingDelegate: recordingDelegate)
-    }
-    
-    func stopRecording(){
-        videoFileOutput.stopRecording()
-        print("Stop Recording: \(videoFileOutput.isRecording)")
-    }
 
+
+/// This extension is used for streaming frames for machine learning
+extension CameraPreviewView: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func setupVideoDataOutput() {
+        // setup video output
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        // If running slowly, set the dispatch to run on the main UI thread
+        let queue = DispatchQueue(label: "com.queue.videosamplequeue")
+        videoDataOutput.setSampleBufferDelegate(self, queue: queue)
+        guard captureSession!.canAddOutput(videoDataOutput) else {
+            fatalError()
+        }
+        captureSession!.addOutput(videoDataOutput)
+        videoConnection = videoDataOutput.connection(with: .video)
+    }
     
+    // TODO: Stop Buffer once person is found
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if connection.videoOrientation != .portrait {
+            connection.videoOrientation = .portrait
+            return
+        }
     
-    
-    
+        if let imageBufferHandler = imageBufferHandler {
+            imageBufferHandler(sampleBuffer)
+            
+//            DispatchQueue.main.async {
+//                self.bridgeModel?.personInView = true // << assign here as needed
+            // TODO: When personIsInView, Kill resources and open VideoRecorder
+//            }
+        }
+    }
 }
 
 
 
 
-//struct VideoPreviewView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        VideoPreviewView()
-//    }
-//}
+struct VideoPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoRecordingView(model: RecordingState())
+    }
+}
+
+
+struct CameraStreamView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoRecordingView(model: RecordingState())
+    }
+}
